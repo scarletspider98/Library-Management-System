a5b5231a5d02a6707831f735ec49b9dcf53291af
Vivek Patel <patelvivek27@gmail.com>
2019-12-06 00:12:48 -0500

    vivek7

---
+++ src/Member.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Member {
+	
+	private int IDNum;
+	private String Name, Email;
+	
+	public Member(){
+		IDNum = 0;
+		Name = null;
+		Email = null;
+	}
+	
+	public Member(int IDNum, String Name, String Email){
+		this.IDNum = IDNum;
+		this.Name = Name;
+		this.Email = Email;
+	}
+	
+	@Override
+	public String toString(){
+		return "\nName: " + Name + "\nEmail: " + Email +
+				"\nIDNum: " + IDNum  + "\n";
+	}
+
+}
---
+++ src/Member_database.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+
+public class Member_database extends Object  {
+	
+	private List<Member> collection;
+
+	public Member_database(){
+		collection = new ArrayList<Member>();
+	}
+	
+	public void addMember(Member book){
+		collection.add(book);
+	}
+	
+	@Override
+	public String toString() {
+		String total = "\n";
+		/*
+		for (int i=0; i<collection.size(); i++){
+			Book b = collection.get(i);
+			total = total + b.toString();
+			
+		}
+		*/
+		Iterator<Member> i = collection.iterator();
+		while(i.hasNext()){
+			Member b = (Member) i.next();
+			total = total + b.toString();
+		}
+		return total;
+	}
+
+}
